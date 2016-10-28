717d2047d0898fbdfba4bb0da2d6ba7bb1ef26b7
winterking3 <439288051@qq.com>
2016-10-28 11:14:02 +0800

    添加备注

--- piggg/src/com/piggg/oa/core/SqlHelper.java
+++ piggg/src/com/piggg/oa/core/SqlHelper.java
@@ -2,6 +2,11 @@ package com.piggg.oa.core;
 
 public class SqlHelper {
 
+	/**
+	 * get sql for add a entity
+	 * @param t
+	 * @param dataBaseName
+	 */
 	public static <T> void Add(T t, String dataBaseName)
 	{
 		String sql = new String();
